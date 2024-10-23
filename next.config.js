95e415711e267d15a00d83f21ce468e5f3782b36
kylesullyman <kylsullivan@chapman.edu>
2024-10-22 23:01:36 -0700

    fixed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // next.config.js
 module.exports = {
     output: 'export',  // Use this for static export
-    basePath: '/resume',
     images: {
         unoptimized: true, // Disable image optimization
     },
