0c7879247fd9a8e0cd53598be0db837ed84b2cbb
kylesullyman <kylsullivan@chapman.edu>
2024-10-22 22:34:46 -0700

    build

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+module.exports = {
+    output: 'export',  // Use this for static export
+    images: {
+        unoptimized: true, // Disable image optimization
+    },
+    // Optionally, set basePath if deploying to a subdirectory
+    // basePath: '/your-repo-name',
+};
